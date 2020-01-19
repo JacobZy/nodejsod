9faeae1b522686882f1e2edd6c0f0e6eb9e44eb4
JacobZy <JacobZy@users.noreply.github.com>
2020-01-19 21:47:09 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+// 服务器端实现
+const fs = require('fs')
+const tls = require('tls')
+const net = require('net')
+const proxy = require('proxy') // 记得将proxy添加到你的package.json依赖中
+
+// 1. 创建一个TLS服务来加密数据 (记得签发你自己的证书)
+const options = {
+  key: fs.readFileSync('./keys/across.key'),
+  cert: fs.readFileSync('./keys/across.pem'),
+}
+const server = tls.createServer(options, (socket) => {
+  socket.pause()
+
+  // 连接并转发数据到本机的代理服务器
+  const remoteSocket = net.connect(3738, '127.0.0.1', () => {
+    socket.pipe(remoteSocket)
+    remoteSocket.pipe(socket)
+
+    socket.resume()
+  })
+
+  socket.on('error', () => {})
+  socket.on('end', () => {})
+})
+
+// 2. 创建一个本机的代理服务器(站在巨人的肩膀上)
+const proxyServer = proxy()
+proxyServer.listen(3738, '127.0.0.1', () => {
+  const port = proxyServer.address().port
+  console.log('proxy server listening on port %d', port)
+})
+
+// 3. 启动TLS服务
+server.listen(37637, () => {
+  console.log('server bound 37637')
+})
